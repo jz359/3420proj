23573391848263b917bb0112e129c50477ef30b2
Andy Zhang <jz359@cornell.edu>
2018-05-02 21:19:41 -0400

    accelerometer code; to be tested with putty

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <Board_Accelerometer.h>
+#include <Board_Magnetometer.h>
+#include <fsl_debug_console.h>
+#include <board.h>
+
+ACCELEROMETER_STATE state;
+MAGNETOMETER_STATE mstate;
+
+int main() {
+	hardware_init();
+	Accelerometer_Initialize();
+	Magnetometer_Initialize();
+
+	while(1) {
+		Accelerometer_GetState(&state);
+		Magnetometer_GetState(&mstate);
+		debug_prinf("%5d %5d %5d %5d %5d %5d\r\n", state.x, state.y, state.z);
+	}
+}
