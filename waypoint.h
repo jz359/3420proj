12a905023677348e37e15362968f3281a78c48d1
Andy Zhang <jz359@cornell.edu>
2018-05-08 17:07:49 -0400

    waypoint header

---
+++ waypoint.h
@@ -0,0 +1,29 @@
+#include <fsl_device_registers.h>
+#include <stdlib.h>
+#include <math.h>
+#include "board_accelerometer.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+
+#include "space.h"
+#include "constants.h"
+
+#define PI 3.14159265
+
+typedef struct waypoint_type {
+	vector* pos;
+	float def_radius;
+	float near_radius;
+	bool is_hit;
+
+	waypoint* next;
+} waypoint;
+
+// pointer to the nearest waypoint; constantly update
+volatile extern waypoint* nearest_waypoint;
+volatile extern waypoint* waypoint_head;
+
+vector* init_waypoint();
+vector* update_nearest_waypoint(void);
+bool is_near_waypoint(void);
+void hit_waypoint(waypoint* wp);
